1f377cee049441618eb0e5ee1a2a5ab3f590709e
Yauhen Ilyin <learning.eugene.ilyin@gmail.com>
2023-05-24 22:29:36 +0300

    first commit

---
+++ src/HelloItGirls.java
@@ -0,0 +1,17 @@
+public class HelloItGirls {
+
+    public static void main(String[] args) {
+        String myName = args[0];
+        System.out.println("Hello, " + myName + "!");
+        System.out.println("Hello, Oleg!");
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello, Friend!");
+    }
+
+    public static void sayHello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,29 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("Hello, Mary!!!");
+        System.out.println("Hello, Fred!!!");
+        System.out.println("rrr");
+    }
+
+}
